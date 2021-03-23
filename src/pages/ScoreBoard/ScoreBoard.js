0d32e483e5e5d4619b0c8ca0e8b5cca89f3c80c1
ermanzohre <ermanzohre@gmail.com>
2021-03-23 09:49:38 +0300

    scoreboard is updated.

--- src/pages/ScoreBoard/ScoreBoard.js
+++ src/pages/ScoreBoard/ScoreBoard.js
@@ -4,6 +4,7 @@ import {useHistory} from 'react-router-dom';
 import './ScoreBoard.css';
 
 const ScoreBoard = () => {
+
   const history = useHistory();
 
   const players = useSelector((state) => state.players);
@@ -13,6 +14,7 @@ const ScoreBoard = () => {
   const handleClick = (e) => {
     history.push('/');
   };
+  
 
   return (
     <div className='end-game'>
